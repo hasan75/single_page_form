1c880867d73a2f6ffff9efc030ef6bbcea6cd89d
hasan75 <hasanjab14@gmail.com>
2023-01-29 02:07:32 +0600

    form page and result page structured for #1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Form from './Pages/Form';
+import FormResult from './Pages/FormResult';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
-          <Route path='/' element={}></Route>
-          <Route path='/result' element={}></Route>
-
+        <Route path='/' element={<Form />}></Route>
+        <Route path='/result' element={<FormResult />}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/FormResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormResult = () => {
+    return (
+        <div>
+            <h2>Form Rtesult</h2>
+        </div>
+    );
+};
+
+export default FormResult;
